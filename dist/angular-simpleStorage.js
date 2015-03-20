044d2b438d4e03c7349b280e7cc6dd84d28c72db
Bill Brady <bill@thevikingcoder.com>
2015-03-19 22:03:07 -0400

    Updated to correct a bug when using localStorage

--- dist/angular-simpleStorage.js
+++ dist/angular-simpleStorage.js
@@ -17,222 +17,274 @@
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  SOFTWARE.
  */
- 
+
 /*
-Author  : Bill Brady <billbsquared@gmail.com>
-Version : 0.1
-*/
+ Author  : Bill Brady <billbsquared@gmail.com>
+ Version : 1.0
+ */
 
 'use strict';
 
-(function () {
+angular.module('storageFactory', [])
+    .factory('$local', function ($rootScope, $window) {
 
-    /**
-     * @ngdoc overview
-     * @name simpleStorage
-     */
-
-    angular.module('simpleStorage', []).
-
-    /**
-     * @ngdoc object
-     * @name simpleStorage.$local
-     * @requires $rootScope
-     * @requires $window
-     */
-
-        factory('$local', _simpleStorage('local')).
-
-    /**
-     * @ngdoc object
-     * @name simpleStorage.$session
-     * @requires $rootScope,
-     * @requires $window
-     */
-
-        factory('$session', _simpleStorage('session')).
-
-    /**
-     * @ngdoc object
-     * @name simpleStorage.
-     * @requires $rootScope
-     * @requires @window
-     */
-
-        factory('$flash', _flashStorage());
-
-    function parseDate(dt) {
+        var $localFactory = {};
+        var method = $window.localStorage;
+        var pre = 'sS.expire.';
 
         /**
-         * Defaults to milliseconds
+         * Creates a localStorage variable
+         * @param k
+         * @param v
+         * @param expire
+         * @returns {boolean}
          */
+        $localFactory.set = function (k, v, expire) {
+            method && method.setItem(k, checkObj(v));
 
-        var dtX = dt.split(' '),
-            lng = parseInt(dtX[0]),
-            method = dtX.length > 1 ? dtX[1] : null,
-            $today = new Date();
-
-        if (['year', 'years'].indexOf(method) > -1) {
-            $today.setDate($today.getDate() + lng);
-        }
-        else if (['month', 'months'].indexOf(method) > -1) {
-            $today.setMonth($today.getMonth() + lng);
-        }
-        else if (['day', 'days'].indexOf(method) > -1) {
-            $today.setDate($today.getDate() + lng);
-        }
-        else if (['hour', 'hours'].indexOf(method) > -1) {
-            $today.setHours($today.getHours() + lng);
-        }
-        else if (['minute', 'minutes'].indexOf(method) > -1) {
-            $today.setMinutes($today.getMinutes() + lng);
-        }
-        else if (['second', 'seconds'].indexOf(method) > -1) {
-            $today.setSeconds($today.getSeconds() + lng);
-        }
-        else {
-            $today.setMilliseconds($today.getMilliseconds() + lng);
-        }
-
-        return $today;
-
-    }
-
-    function checkJson(el) {
-
-        var response;
-
-        try {
-            response = JSON.parse(el);
-        }
-        catch (e) {
-            response = el;
-        }
-
-        return response;
-
-    }
-
-    function checkObj(el) {
-
-        if (typeof el == "object") {
-            return angular.toJson(el);
-        }
-
-        return el;
-
-    }
-
-    function _simpleStorage(method) {
-
-        return [
-
-            '$rootScope', '$window',
-
-            function ($rootScope, $window) {
-
-                method = method == 'local' ? $window.localStorage : $window.sessionStorage;
-                var pre = 'sS.expire.';
-
-                return {
-
-                    set: function (k, v, expire) {
-
-                        method && method.setItem(k, checkObj(v));
-
-                        if (typeof(expire) != 'undefined') {
-                            k = pre + k;
-                            method && method.setItem(k, parseDate(expire));
-                        }
-
-                        return this;
-
-                    },
-                    get: function (k) {
-
-                        if (method && method.getItem(pre + k)) {
-
-                            var expDate = new Date(method && method.getItem(pre + k)).getTime(),
-                                $now = new Date().getTime();
-
-                            if (expDate < $now) {
-                                method && method.removeItem(k);
-                                method && method.removeItem(pre + k);
-                                return null;
-                            }
-
-                        }
-
-                        return checkJson(method && method.getItem(k));
-
-                    },
-                    getExp: function (k) {
-
-                        if (method && method.getItem(pre + k)) {
-                            return method && method.getItem(pre + k);
-                        }
-
-                        return null;
-
-                    },
-                    remove: function (k) {
-                        method && method.removeItem(k);
-                    },
-                    clear: function () {
-                        method && method.clear();
-                    },
-                    all: function () {
-                        var i = 0,
-                            oJson = {},
-                            sKey;
-                        for (; sKey = method.key(i); i++) {
-                            oJson[sKey] = method.getItem(sKey);
-                        }
-                        return oJson;
-                    }
-
-                }
-
+            if (typeof expire != "undefined") {
+                k = pre + k;
+                method && method.setItem(k, parseDate(expire));
             }
 
-        ]
+            return !!this.get(k);
+        };
 
-    }
+        /**
+         * Returns a localStorage value
+         * @param k
+         * @returns {*}
+         */
+        $localFactory.get = function (k) {
+            if (method && method.getItem(pre + k)) {
+                var expDate = new Date(method && method.getItem(pre + k)).getTime(),
+                    $now = new Date().getTime();
 
-    function _flashStorage() {
-
-        return [
-
-            '$rootScope', '$window',
-
-            function ($rootScope, $window) {
-
-                var pre = 'sS.flash.',
-                    method = $window.localStorage;
-
-                return {
-
-                    set: function (k, v) {
-                        k = pre + k;
-                        method && method.setItem(k, checkObj(v));
-                    },
-                    get: function (k) {
-                        k = pre + k;
-                        var val = checkJson(method && method.getItem(k));
-                        method && method.removeItem(k);
-                        return val;
-                    },
-                    remove: function (k) {
-                        k = pre + k;
-                        method && method.remove(k);
-                    }
-
+                if (expDate < $now) {
+                    method && method.removeItem(k);
+                    method && method.removeItem(pre + k);
+                    return null;
                 }
-
             }
 
-        ]
+            return checkJson(method && method.getItem(k));
+        };
 
+        /**
+         * Removes a localStorage item by key
+         * @param k
+         */
+        $localFactory.remove = function (k) {
+            method && method.removeItem(k);
+        };
+
+        /**
+         * Removes all localStorage items
+         */
+        $localFactory.clear = function () {
+            method && method.clear();
+        };
+
+        /**
+         * A json string of all items in localStorage
+         * @returns {{}}
+         */
+        $localFactory.all = function () {
+            var i = 0,
+                oJson = {},
+                sKey;
+            for (; sKey = method.key(i); i++) {
+                oJson[sKey] = method.getItem(sKey);
+            }
+
+            return oJson;
+        };
+
+        return $localFactory;
+
+    })
+    .factory('$session', function ($rootScope, $window) {
+
+        var $sessionFactory = {};
+        var method = $window.sessionStorage;
+        var pre = 'sS.expire.';
+
+        /**
+         * Creates a sessionStorage
+         * @param k
+         * @param v
+         * @param expire
+         * @returns {boolean}
+         */
+        $sessionFactory.set = function (k, v, expire) {
+            method && method.setItem(k, checkObj(v));
+
+            if (typeof expire != "undefined") {
+                k = pre + k;
+                method && method.setItem(k, parseDate(expire));
+            }
+
+            return !!this.get(k);
+        };
+
+        /**
+         * Returns a sessionStorage value by key
+         * @param k
+         * @returns {*}
+         */
+        $sessionFactory.get = function (k) {
+            if (method && method.getItem(pre + k)) {
+                var expDate = new Date(method && method.getItem(pre + k)).getTime(),
+                    $now = new Date().getTime();
+
+                if (expDate < $now) {
+                    method && method.removeItem(k);
+                    method && method.removeItem(pre + k);
+                    return null;
+                }
+            }
+
+            return checkJson(method && method.getItem(k));
+        };
+
+        /**
+         * Removes a sessionStorage item by key
+         * @param k
+         */
+        $sessionFactory.remove = function (k) {
+            method && method.removeItem(k);
+        };
+
+        /**
+         * Removes all sessionStorage items
+         */
+        $sessionFactory.clear = function () {
+            method && method.clear();
+        };
+
+        /**
+         * A json string of all items in sessionStorage
+         * @returns {{}}
+         */
+        $sessionFactory.all = function () {
+            var i = 0,
+                oJson = {},
+                sKey;
+            for (; sKey = method.key(i); i++) {
+                oJson[sKey] = method.getItem(sKey);
+            }
+
+            return oJson;
+        };
+
+        return $sessionFactory;
+
+    })
+    .factory('$flash', function ($rootScope, $window) {
+
+        var $flashFactory = {},
+            pre = 'sS.flash.',
+            method = $window.localStorage;
+        /**
+         * Creates an item in flash storage
+         * @param k
+         * @param v
+         */
+        $flashFactory.set = function (k, v) {
+            method && method.setItem(pre + k, checkObj(v));
+        };
+
+        /**
+         * Retrieves a value stored in the flash storage
+         * @param k
+         * @returns {*}
+         */
+        $flashFactory.get = function (k) {
+            var val = checkJson(method && method.getItem(pre + k));
+            method && method.removeItem(pre + k);
+            return val;
+        };
+
+        /**
+         * Removes an item in flash storage
+         * @param k
+         */
+        $flashFactory.remove = function (k) {
+            method && method.removeItem(pre + k);
+        };
+
+        return $flashFactory;
+
+    });
+
+/**
+ *
+ * @param el
+ * @returns {*}
+ */
+function checkObj(el) {
+    if (typeof el == "object") {
+        return angular.toJson(el);
     }
 
-})();
+    return el;
+}
+
+/**
+ *
+ * @param el
+ * @returns {*}
+ */
+function checkJson(el) {
+    var response;
+
+    try {
+        response = JSON.parse(el);
+    } catch (e) {
+        response = el;
+    }
+
+    return response;
+}
+
+/**
+ *
+ * @param dt
+ * @returns {Date}
+ */
+function parseDate(dt) {
+
+    /**
+     * Defaults to milliseconds
+     */
+
+    var dtX = dt.split(' '),
+        lng = parseInt(dtX[0]),
+        method = dtX.length > 1 ? dtX[1] : null,
+        $today = new Date();
+
+    if (['year', 'years'].indexOf(method) > -1) {
+        $today.setDate($today.getDate() + lng);
+    }
+    else if (['month', 'months'].indexOf(method) > -1) {
+        $today.setMonth($today.getMonth() + lng);
+    }
+    else if (['day', 'days'].indexOf(method) > -1) {
+        $today.setDate($today.getDate() + lng);
+    }
+    else if (['hour', 'hours'].indexOf(method) > -1) {
+        $today.setHours($today.getHours() + lng);
+    }
+    else if (['minute', 'minutes'].indexOf(method) > -1) {
+        $today.setMinutes($today.getMinutes() + lng);
+    }
+    else if (['second', 'seconds'].indexOf(method) > -1) {
+        $today.setSeconds($today.getSeconds() + lng);
+    }
+    else {
+        $today.setMilliseconds($today.getMilliseconds() + lng);
+    }
+
+    return $today;
+
+}
